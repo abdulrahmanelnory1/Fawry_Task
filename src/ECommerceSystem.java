effdd9e57fab8e53dfb203d1fab959ff93203401
Abdulrahman Elnory <135131957+abdulrahmanelnory1@users.noreply.github.com>
2025-07-05 13:26:50 +0300

    Update ECommerceSystem.java

--- src/ECommerceSystem.java
+++ src/ECommerceSystem.java
@@ -10,23 +10,20 @@ public class ECommerceSystem {
         Product product1 = new Product("mobile", 100, 2);
         Product product2 = new ShippableProduct("cheese", 150, 3, 200);
         Product product3 = new ShippableAndExpirableProduct("TV", 400, 2, LocalDate.now().plusYears(1), 1300);
+        Product product4 = new ExpirableProduct("Radio", 400, 2, LocalDate.now().plusYears(1));
 
         Cart cart1 =  new Cart();
         Cart cart2 =  new Cart();
 
-        cart1.add(product1, 1);
+        cart1.add(product4, 1);
         cart1.add(product2, 2);
         cart2.add(product3, 1);
         cart2.add(product2, 1);
 
-        Customer customer1 = new Customer("Ahmed", 1000, cart1);
-        Customer customer2 = new Customer("Tarek", 1000, cart2);
+        Customer customer1 = new Customer("Ahmed", 1000);
+        Customer customer2 = new Customer("Tarek", 1000);
 
-        ShippingService.shippingService(cart2);
-        Checkout.checkout(customer2, cart2);
-
-
-
-
+        ShippingService.shippingService(cart1);
+        Checkout.checkout(customer1, cart1);
     }
 }

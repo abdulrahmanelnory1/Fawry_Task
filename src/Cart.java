e42cd63fea40d99b5638d2b192aac2cec94eb975
Abdulrahman Elnory <135131957+abdulrahmanelnory1@users.noreply.github.com>
2025-07-07 07:27:46 +0300

    Update Cart.java

--- src/Cart.java
+++ src/Cart.java
@@ -2,19 +2,23 @@ import java.util.HashMap;
 
 public class Cart {
 
-
     /** - Represents the product name as a key and the amount as a value.
      *  - Duplicated products is not allowed.
      */
-    HashMap<Product,Integer> products =  new HashMap<>();
+    private HashMap<Product,Integer> products =  new HashMap<>();
 
-    public void add(Product product, Integer quantity) {
+    public void add(Product product, Integer amount) {
 
         // if the product is out of the stock.
-        if(product.getQuantity() < quantity)
+        if(product.getQuantity() < amount)
             throw new IllegalArgumentException("There is not enough quantities for " + product.getName());
 
-        products.put(product, quantity );
+        if (product instanceof Expirable && ((Expirable) product).isExpired())
+            throw new IllegalStateException("ERROR: The product is expired.");
+
+        // Reducing the available quantity.
+        product.updateQuantity(amount) ;
+        products.put(product, amount);
     }
 
     public HashMap<Product,Integer> getProducts(){

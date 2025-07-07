ffd6ac2030097085ee6235b78a006aa8c99106ed
Abdulrahman Elnory <135131957+abdulrahmanelnory1@users.noreply.github.com>
2025-07-07 07:30:18 +0300

    Update ExpirableProduct.java

--- src/ExpirableProduct.java
+++ src/ExpirableProduct.java
@@ -3,18 +3,13 @@ import java.util.Date;
 
 public class ExpirableProduct extends Product implements Expirable {
 
-    LocalDate expirationDate;
+    private LocalDate expirationDate;
 
     public  ExpirableProduct(String name, double price, int quantity, LocalDate expirationDate) {
         super(name, price, quantity);
         this.expirationDate = expirationDate;
     }
-
-    @Override
-    public String getName(){
-        return name;
-    }
-
+    // Check if the product is Expired or available to use.
     @Override
     public boolean isExpired(){
         return expirationDate.isBefore(LocalDate.now());
